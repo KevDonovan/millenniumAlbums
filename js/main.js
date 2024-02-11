ad03b74cc774addd3d20b84e3406d7de99dd4959
Kevin Donovan <kevdonovan88@gmail.com>
2024-02-10 20:24:41 -0600

    upaded main.js for heroku url

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest);
 async function apiRequest() {
     const albumName = document.querySelector('input').value;
     try{
-        const res = await fetch(`/api/${albumName}`);
+        const res = await fetch(`https://album-server-620a730b8161.herokuapp.com/api/${albumName}`);
         const data = await res.json();
         console.log(data);
     } catch(error){
